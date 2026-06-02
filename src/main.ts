cbe513e37d0f9f22b0e54330b9eb724923fb0c24
Mohammad Hamza <mhfrough@yahoo.com>
2026-06-03 00:05:57 +0500

    port

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, SwaggerModule.createDocument(app, swaggerConfig));
 
   const port = process.env.PORT ?? 3023;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Backend running on http://localhost:${port}/api/v1`);
   console.log(`📚 Swagger docs  → http://localhost:${port}/api/docs`);
 }
